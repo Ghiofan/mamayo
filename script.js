3d1d99f5b771b286906ff2589282e2b3e7b3e4e2
Ghiofan <mohammadghiofan@gmail.com>
2021-12-05 17:05:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 2.3,
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: false,
+    dots: false,
+  });
+});
